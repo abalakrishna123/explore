fixed bug in target values computation

pytorch-ddpg/ddpg.py
@@ -73,7 +73,7 @@ class DDPG(object):
         next_q_values.volatile=False
 
         target_q_batch = to_tensor(reward_batch) + \
-            self.discount*to_tensor(1.0 - terminal_batch.astype(np.float))*next_q_values
+            self.discount*to_tensor(terminal_batch.astype(np.float))*next_q_values
 
         # Critic update
         self.critic.zero_grad()
@@ -122,12 +122,15 @@ class DDPG(object):
         self.a_t = action
         return action
 
-    def select_action(self, s_t, decay_epsilon=True):
+    def select_action(self, s_t, theta, data, decay_epsilon=True):
         action = to_numpy(
             self.actor(to_tensor(np.array([s_t])))
         ).squeeze(0)
-        action += self.is_training*max(self.epsilon, 0)*self.random_process.sample()
-        action = np.clip(action, -0.01., 0.01.)
+        # action += self.is_training*max(self.epsilon, 0)*self.random_process.sample()
+        # HACK: Make all exploring just following gradients
+        action += self.is_training*max(self.epsilon, 0)*SGD_linear_loss(theta, 0.001, data[np.random.randint(len(data))])
+        # action = np.clip(action, -0.001, 0.001)
+        action = np.clip(action, -0.01, 0.01)
 
         if decay_epsilon:
             self.epsilon -= self.depsilon

pytorch-ddpg/main.py
@@ -10,6 +10,8 @@ from normalized_env import NormalizedEnv
 from evaluator import Evaluator
 from ddpg import DDPG
 from util import *
+from scipy.io import savemat
+import matplotlib.pyplot as plt
 
 def train(num_iterations, agent, env,  evaluate, validate_steps, output, max_episode_length=None, debug=False):
 
@@ -18,6 +20,7 @@ def train(num_iterations, agent, env,  evaluate, validate_steps, output, max_epi
     step = episode = episode_steps = 0
     episode_reward = 0.
     observation = None
+    episode_rewards = []
     while step < num_iterations:
         # reset if it is the start of episode
         if observation is None:
@@ -42,10 +45,10 @@ def train(num_iterations, agent, env,  evaluate, validate_steps, output, max_epi
             agent.update_policy()
         
         # [optional] evaluate
-        if evaluate is not None and validate_steps > 0 and step % validate_steps == 0:
-            policy = lambda x: agent.select_action(x, decay_epsilon=False)
-            validate_reward = evaluate(env, policy, debug=False, visualize=False)
-            if debug: prYellow('[Evaluate] Step_{:07d}: mean_reward:{}'.format(step, validate_reward))
+        # if evaluate is not None and validate_steps > 0 and step % validate_steps == 0:
+        #     policy = lambda x: agent.select_action(x, decay_epsilon=False)
+        #     validate_reward = evaluate(env, policy, debug=False, visualize=False)
+        #     if debug: prYellow('[Evaluate] Step_{:07d}: mean_reward:{}'.format(step, validate_reward))
 
         # [optional] save intermideate model
         if step % int(num_iterations/3) == 0:
@@ -58,6 +61,21 @@ def train(num_iterations, agent, env,  evaluate, validate_steps, output, max_epi
         observation = deepcopy(observation2)
 
         if done: # end of episode
+
+            episode_rewards.append( episode_reward/float(episode_steps)  )
+
+            if episode % 10 == 0:                 
+                x = np.arange(len(episode_rewards))
+                y = np.array(episode_rewards)
+                plt.xlabel('Episode')
+                plt.ylabel(' Average Reward')
+                plt.plot(x, y)
+                plt.savefig('{}/episode_reward'.format(output) +'.png')
+                savemat('{}/episode_reward'.format(output) +'.mat', {'reward':episode_rewards})
+                plt.clf()
+                plt.cla()
+                plt.close()
+
             if debug: prGreen('#{}: episode_reward:{} steps:{}'.format(episode,episode_reward,step))
 
             agent.memory.append(

pytorch-ddpg/opt_main.py
@@ -11,6 +11,7 @@ from evaluator import Evaluator
 from ddpg import DDPG
 from util import *
 from scipy.io import savemat
+import matplotlib.pyplot as plt
 
 def train(num_iterations, agent, env,  evaluate, validate_steps, output, max_episode_length=None, debug=False):
 
@@ -35,7 +36,7 @@ def train(num_iterations, agent, env,  evaluate, validate_steps, output, max_epi
             # For now when exploring, follow what SGD would do:
             action = agent.SGD_action(env.get_theta(), env.get_data())
         else:
-            action = agent.select_action(observation)
+            action = agent.select_action(observation, env.get_theta(), env.get_data())
         
         # env response with next_observation, reward, terminate_info
         observation2, reward, done, loss = env.step(action)
@@ -107,7 +108,7 @@ def train(num_iterations, agent, env,  evaluate, validate_steps, output, max_epi
 
             agent.memory.append(
                 observation,
-                agent.select_action(observation),
+                agent.select_action(observation, env.get_theta(), env.get_data()),
                 0., False
             )
 